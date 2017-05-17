8565b14cccb4e664fde1bb28ec3be61f3af50489
Jan Mercl <0xjnml@gmail.com>
2017-05-18 00:33:53 +0200

    Adjust for latest irgo/opt.go
    	modified:   all_test.go

--- all_test.go
+++ all_test.go
@@ -614,9 +614,6 @@ func TestGCCExec(t *testing.T) {
 		// un-flatten (wips wrt cc.0506a942f3efa9b7a0a4b98dbe45bf7e8d06a542)
 		"20030714-1.c": {}, // cc.Parse: ../cc/testdata/gcc-6.3.0/gcc/testsuite/gcc.c-torture/execute/20030714-1.c:102:11: assignment from incompatible type ('unsigned' = '<undefined>')
 		"anon-1.c":     {}, // cc.Parse: ../cc/testdata/gcc-6.3.0/gcc/testsuite/gcc.c-torture/execute/anon-1.c:22:7: struct{int; ;} has no member named b
-
-		"20021120-1.c": {}, // irgo.opt does not finish.
-		"pr28982a.c":   {}, // irgo.opt does not finish.
 	}
 	wd, err := os.Getwd()
 	if err != nil {
